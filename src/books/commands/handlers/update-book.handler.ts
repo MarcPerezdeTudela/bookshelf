5febef433fad5579a9e1739bc650df4d4f616877
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2023-03-10 19:21:50 +0100

    fix: change PrismaClient for PrismaService in UpdateBookDTO

--- src/books/commands/handlers/update-book.handler.ts
+++ src/books/commands/handlers/update-book.handler.ts
@@ -1,11 +1,11 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { PrismaClient } from '@prisma/client';
 import { BookEntity } from 'src/books/domain/models';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateBookCommand } from '../impl';
 
 @CommandHandler(UpdateBookCommand)
 export class UpdateBookHandler implements ICommandHandler<UpdateBookCommand> {
-  constructor(private readonly prisma: PrismaClient) {}
+  constructor(private readonly prisma: PrismaService) {}
   execute(command: UpdateBookCommand): Promise<BookEntity> {
     return this.prisma.book.update({
       data: command.updateBookDto,
